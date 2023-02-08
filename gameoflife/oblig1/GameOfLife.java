46cb73ef6ea31ef494e660d115dbf27a9021e6f7
Fredrik Foss-Indrehus <frdrkfi@gmail.com>
2023-02-08 15:30:55 +0100

    first commit

---
+++ gameoflife/oblig1/GameOfLife.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Klasse GameOfLife
+public class GameOfLife {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Antall rader:");
+        int antRader = scanner.nextInt();
+
+        System.out.println("Antall kolonner");
+        int antKolonner = scanner.nextInt();
+
+        Verden verden = new Verden(antRader, antKolonner);
+        verden.tegn();
+
+        boolean fortsette = true;
+        while (fortsette) {
+            System.out.println("Fortsette? ['enter'/n]:");
+            String input = scanner.nextLine();
+
+            if (input == "") {
+                verden.oppdatering();
+                verden.tegn();
+            }
+            else {
+                fortsette = false;
+            }
+        }
+    }
+}
---
+++ lesFil.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.Scanner;
+
+// Klasse som leser fil og printer ut i terminal
+class LesFil {
+    public static void main(String[] args) {
+        Scanner f = null;
+        try {
+            f = new Scanner(new File("tall-til-10.txt"));
+        } catch (Exception e) {
+            System.out.println("Kan ikke lese filen tall-til-10.txt");
+            System.exit(1);
+        }
+
+        // Les datafilen og skriv ut til terminal
+        while (f.hasNextLine()) {
+            System.out.println(f.nextLine());
+        }
+        f.close();
+    }
+}
